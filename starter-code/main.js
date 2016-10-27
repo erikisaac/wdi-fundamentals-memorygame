83103cc0ae4e3ac521d11a9376675b313681a587
erikisaac <erik@erikisaac.com>
2016-10-27 11:07:34 -0600

    Added the if then

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,11 @@ var card2 = 'king';
 var card3 = 'queen';
 var card4 = 'queen';
 
+var firstCardChosen = card1;
+var secondCardChosen = card2;
+
+if (firstCardChosen === secondCardChosen) {
+	alert('You found a match!');
+	} else {
+		alert('Sorry, try again.');
+	}
