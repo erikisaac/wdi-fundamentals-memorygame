9974176d1a68c7f9360c408b68f205f97ad42f7c
erikisaac <erik@erikisaac.com>
2016-11-01 19:25:05 -0600

    This is the update before the due time but it's comopletly broken.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,21 +1,130 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 
-var card1 = 'king';
-var card2 = 'king';
-var card3 = 'queen';
-var card4 = 'queen';
+// var card1 = 'king';
+// var card2 = 'king';
+// var card3 = 'queen';
+// var card4 = 'queen';
 
-var firstCardChosen = card1;
-var secondCardChosen = card2;
+// var cards = ['queen', 'queen', 'king', 'king'];
 
-// if (firstCardChosen === secondCardChosen) {
-// 	alert('You found a match!');
+// var cardsInPlay = [];
+
+// // var firstCardChosen = card1;
+// // var secondCardChosen = card2;
+
+// // if (firstCardChosen === secondCardChosen) {
+// // 	alert('You found a match!');
+// // 	} else {
+// // 		alert('Sorry, try again.');
+// // 	}
+
+// var board = document.getElementById('game-board');
+
+// function makeBoard() {
+// 	for (var i=0; i<cards.length; i++) {
+// 		var cardElement = document.createElement('div');
+// 		cardElement.className = 'card';
+// 		cardElement.setAttribute('data-card', cards[i]);
+// 		cardElement.addEventsListener('click', isTwoCards);
+
+// 		board.appendChild(cardElement);
+// 		board.appendChild(cardElement);
+// 	}
+// }
+
+// function isTwoCards() {
+
+//   cardsInPlay.push(this.getAttribute('data-card'));
+
+// 	console.log(this.getAttribute('data-card'));
+// 	if (this.getAttribute('data-card') === 'king') {
+// 		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>";
 // 	} else {
-// 		alert('Sorry, try again.');
+// 		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>";
 // 	}
 
-var board = document.getElementById('game-board');
+//   if (cardsInPlay.length === 2) {
 
-function makeBoard() {
-	for ()
+//     isMatch(cardsInPlay);
+
+//     cardsInPlay = [];
+//   }
+// }
+
+// function isMatch(cards) {
+
+//   if (cards[0] === cards[1]) {
+//     alert("You found a match!");
+//   } else {
+//     alert("Sorry, not a match.");
+
+//   }
+// }
+
+// makeBoard();
+
+// represents the cards of the game
+var cards = ['queen', 'queen', 'king', 'king'];
+
+// represents the cards in play
+var cardsInPlay = [];
+
+
+
+// find the board and set it to a variable
+// so that we can add cards to it
+var board = document.getElementById('game-board');
+// function that will create your board
+function createBoard() {
+  // loop through your cards array to create card elements for your board
+  for (var i=0; i<cards.length; i++) {
+    // create a div element which will be used as a card
+    var cardElement = document.createElement('div');
+    // add a class to the card element which will help link styling
+    cardElement.className = 'card';
+		// this will set the card's 'data-card' to be the element of the array
+    // data- attributes are meant to store data about an element that is not tied to a style.
+    // i.e. "king"
+    cardElement.setAttribute('data-card', cards[i]);
+		// when a card is clicked the function isTwoCards will be executed
+    cardElement.addEventListener('click', isTwoCards);
+
+    // append the card to the board
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  }
+
 }
+//checks to see if there are cards in play
+function isTwoCards() {
+  // add card to array of cards being viewed
+  // 'this' is not covered in the pre-work but
+  // for now, just know it gives you access to the cardElement you click on
+  cardsInPlay.push(this.getAttribute('data-card'));
+	// show the card's image
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>"; // king
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; //queen
+	}
+  // if you have two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+    // clear cards in play array for next try
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+  // alert winning message
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+}
+
+createBoard();
